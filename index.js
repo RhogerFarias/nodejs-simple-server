02351b0406d37fe69369216b74a3246cf31e5b3b
RhogerFarias <rhoger.gabriel.12@gmail.com>
2025-02-04 23:05:38 -0300

    atualizando index.js

--- index.js
+++ index.js
@@ -16,6 +16,11 @@ server.get("/hello", (req, res) => { //req = requisição ; res = resposta
     });
 });
 
+// no outro browser pode chamar isso fazendo apenas:
+// json['title]
+// -> "Hello World"
+
+
 //selecionando a porta - geralmente 3000, 5000, 8000, 8080 
 server.listen(3000);
 

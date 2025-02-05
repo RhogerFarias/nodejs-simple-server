5d4a2b80a9025ac168bb8736030d90feda9fa597
RhogerFarias <rhoger.gabriel.12@gmail.com>
2025-02-04 23:00:58 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// requisição do express
+const express = require("express");
+
+//iniciando o express como servidor
+const server = express();
+
+//construindo uma rota - get = recebe
+server.get("/hello", (req, res) => { //req = requisição ; res = resposta
+    //resposta para o browser
+   // return res.send("Hello World");
+
+   //resposta para o browser em JSON
+   return  res.json({ 
+    title: "Hello World",
+    message: "mensagem 1"
+    });
+});
+
+//selecionando a porta - geralmente 3000, 5000, 8000, 8080 
+server.listen(3000);
+
